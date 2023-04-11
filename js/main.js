1272a01fba05a62427ddf7e1d011384936eec0ab
Paul Christian Tan <paulchristiantan.projects@gmail.com>
2023-04-11 23:52:59 +0800

    Add Hamburger, footer and fix styling for mobile view

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.log("hello");
+const hamburger = document.querySelector(".hamburger")
+const navMenu = document.querySelector(".nav-menu")
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
