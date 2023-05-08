30877fd47e7accd16343ad3ac2ca55a8ed4ea619
Paul Christian Tan <paulchristiantan.projects@gmail.com>
2023-05-08 16:48:31 +0800

    revamp code

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function darkMode() {
+    var element = document.body;
+    var content = document.getElementById("DarkModetext");
+    element.className = "dark-mode";
+  }
+  function lightMode() {
+    var element = document.body;
+    var content = document.getElementById("DarkModetext");
+    element.className = "light-mode";
+  }
